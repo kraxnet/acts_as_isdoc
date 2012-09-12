8839165eeed845cb107783edd57d93de6aaf617a
Josef Pospíšil <josef.pospisil@laststar.eu>
2012-09-12 17:35:53 +0200

    Fix domains extended invoice

--- test/fixtures/extended_invoice.rb
+++ test/fixtures/extended_invoice.rb
@@ -2,11 +2,10 @@
 class ExtendedInvoice < BasicInvoice
   def draw_invoice_line_extensions(invoice_line, item)
     invoice_line.tag! :Extensions do |extensions|
-      extensions.tag! :domain, :xmlns=>"http://www.xnet.cz/xml/isdoc/domain-1.0" do |domain|
-        domain.tag! :fqdn, "fqdn.cz"
-        domain.tag! :since, "2009-01-01"
-        domain.tag! :till, "2010-01-01"
-        domain.tag! :period, "1Y"
+      extensions.tag! :Domains, :xmlns=>"http://www.xnet.cz/xml/isdoc/domain-1.0" do |domain|
+        domain.tag! :Name, "fqdn.cz"
+        domain.tag! :Since, "2009-01-01"
+        domain.tag! :Till, "2010-01-01"
       end
     end
   end
