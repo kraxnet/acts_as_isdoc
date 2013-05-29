c579c466b9a962d2921c255f2bfc72111d0063ea
Josef Pospíšil <josef.pospisil@laststar.eu>
2013-05-29 18:59:33 +0200

    Fix extended invoice fixture

--- test/fixtures/extended_invoice.rb
+++ test/fixtures/extended_invoice.rb
@@ -3,9 +3,9 @@ class ExtendedInvoice < BasicInvoice
   def draw_invoice_line_extensions(invoice_line, item)
     invoice_line.tag! :Extensions do |extensions|
       extensions.tag! :Domains, :xmlns=>"http://www.xnet.cz/xml/isdoc/domain-1.0" do |domain|
-        domain.tag! :Name, item[:name]
-        domain.tag! :Since, item[:since]
-        domain.tag! :Till, item[:till]
+        domain.tag! :Name, "fqdn.cz"
+        domain.tag! :Since, "2009-01-01"
+        domain.tag! :Till, "2010-01-01"
       end
     end
   end
